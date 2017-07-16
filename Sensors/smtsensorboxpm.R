db193a82069b3356114f9821d1afe44dd0b4186a
Marie English <marieenglish@Maries-MacBook-Air.local>
2017-07-16 16:13:32 -0400

    switching

---
+++ Sensors/smtsensorboxpm.R
@@ -0,0 +1,45 @@
+
+#' Soil moisture and temp sensor boxplot (Moisture)
+#'
+#' @param df A csv file from soil moisture/temp sensors
+#' @param sensornames Vector of the names of the sensors used in the order they appear on the csv
+#' @param startdate Beginning of date range of interest (format: "y-m-d")
+#' @param enddate End of date range of interest (format: "y-m-d")
+#' @return Boxplot (a summary of the data for the entered range of dates)
+#' @details This function takes data from Decagon 5TM soil/moisture sensors and returns a box plot of the soil moisture data for the time period requested 
+#' @export 
+#' @examples
+#' smtsensorboxpm(dfsensor, sensornameg, "2015-08-10", "2015-08-20")
+
+smtsensorboxpm <- function(df, sensornames, startdate, enddate) {
+  
+  #Ensure that input is a dataframe or a warning is issued
+  if (is.data.frame(df) == FALSE) {
+    warning ("Input is not a dataframe")} 
+  
+  #Lubridate converts dates as characters to POSIXct from the given df:
+  datetime<- lubridate::dmy_hm(df[[1]], tz= "EST")
+  
+  #Remove the character dates:
+  df_2<- df %>%
+    select (2:9)
+  
+  #Combine parsed dates and new table: 
+  df_3<- cbind(datetime, df_2)
+  
+  #Rename columns 
+  colnames(df_3)[2:9] <- sensornames
+  
+  #Moisture boxplot
+  dfplot<- df_3 %>%
+    filter (datetime >= as.POSIXct(startdate) & datetime <= as.POSIXct(enddate)) %>% 
+    select (3,5,7,9) %>%  #columns containing moisture data 
+    gather (sensor, moisture) %>%  #make wide df long 
+    group_by(sensor) %>%  
+    ggplot(aes(x = sensor, y= moisture)) +
+    geom_boxplot()
+  return(dfplot)
+  
+}
+
+
